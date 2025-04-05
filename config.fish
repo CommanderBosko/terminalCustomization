7389d778a670d5452f2bc184d7a778a7f3d800af
CommanderBosko <kurthoernig@gmail.com>
2025-04-05 15:05:52 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,11 +1,13 @@
+# reference CachyOS Fish config
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
+# overwrite greeting - potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
 
+# initialize Starship
 starship init fish | source
 
+# initialize Pywal
 cat ~/.cache/wal/sequences &
